617fe52bc706ccb7d13be3c93925568f4935e99e
jiroscopes <steven.popick@gmail.com>
2023-04-01 16:33:46 -0500

    improving SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,34 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script';
+import { DefaultSeo } from 'next-seo';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'en_IE',
+            url: 'https://www.wiai.io/',
+            siteName: 'wiai',
+            description: 'Pick which image is AI generated. New images daily!',
+            images: [
+              {
+                url: 'https://wiai.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmockup.7016e3da.png&w=1920&q=100',
+                width: 1920,
+                height: 1080,
+                alt: 'Image of Wiai round 1',
+                type: 'image/png'
+              }
+            ]
+          }}
+          twitter={{
+            handle: '@jiroscopes',
+            site: '@wiai_io',
+            cardType: 'summary_large_image',
+          }}
+      />
       <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-JDVXGE8R2W" />
       <Script 
         id='google-analytics'
